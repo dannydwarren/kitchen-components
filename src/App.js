50f5f207a5f8e9bbbe7195f98a839a0df7d4ee1a
Danny Warren <dannydwarren@gmail.com>
2018-04-11 20:25:41 -0600

    Add router and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import './App.css'
 import Kitchen from './Kitchen'
+import Page1 from './Page1'
+import { BrowserRouter as Router, Route, NavLink as Link, Switch } from 'react-router-dom' //in terminal install via "npm install react-router-dom"
 
 class App extends Component {
-  state = { 
+  state = {
     kitchenName: "Danny's Awesome Kitchen",
     kitchenPowerIsOn: false
   }
@@ -24,9 +26,19 @@ class App extends Component {
   render() {
     console.log('App.render()')
     return (
-      <div className="App">
-        <Kitchen name={this.state.kitchenName} powerIsOn={this.state.kitchenPowerIsOn} />
-      </div>
+      <Router>
+        <div className="App">
+          <nav>
+            <Link exact to="/" activeStyle={{ fontWeight: 'bold', color: 'red' }}>Home for 4</Link>
+            <Link to="/kitchen" activeStyle={{ fontWeight: 'bold', color: 'red' }}>My Super Duper Kitchen</Link>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={Page1} />
+            <Route path="/kitchen" component={() => <Kitchen name={this.state.kitchenName} powerIsOn={this.state.kitchenPowerIsOn} />} />
+            <Route component={() => <div>404 ERROR</div>} />
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
